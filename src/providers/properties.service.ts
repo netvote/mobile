2caf90ceb29c587ba1cc4ed89e559072263f5ad1
Steven Landers <steven.landers@gmail.com>
2017-01-10 15:50:50 -0500

    Update properties.service.ts

--- src/providers/properties.service.ts
+++ src/providers/properties.service.ts
@@ -4,9 +4,9 @@
 
 export let _REGION = "us-east-1";
 
-export let _IDENTITY_POOL_ID = "us-east-1:688daf8f-a234-423e-9222-4d489c796834";
-export let _USER_POOL_ID = "us-east-1_bybDCnVM9";
-export let _CLIENT_ID = "3hl0eo4321kv7i3i2amqa8k8jf";
+export let _IDENTITY_POOL_ID = "us-east-1:7b2efd9b-9d5c-4fa4-a775-ea2a317db99f"";
+export let _USER_POOL_ID = "us-east-1_tOAOIzQcp";
+export let _CLIENT_ID = "5c7at50hr2p8fjfmrggt6iidcs";
 export let _MOBILE_ANALYTICS_APP_ID = "8ed115bcfac94dfa8e364aa5ef201849";
 
 export let _POOL_DATA = {
