2ad59c5377070a638ab16c91695c1ff56dfd3268
slanders <slanders@pindrop.com>
2017-02-02 17:33:50 -0500

    adding deep links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import {Component, ViewChild} from "@angular/core";
-import {Events, NavController, Platform, MenuController} from "ionic-angular";
-import {Splashscreen} from "ionic-native";
+import {Events, Platform, MenuController, Nav} from "ionic-angular";
+import {Splashscreen, Deeplinks} from "ionic-native";
 import {LoginComponent, LogoutComponent} from "../pages/auth/auth";
 import {AwsUtil} from "../providers/aws.service";
 import {VoterBallotListPage} from "../pages/voter-ballot-list/voter-ballot-list";
 import {ManageBallotsPage} from "../pages/manage-ballots/manage-ballots";
 import {UserLoginService} from "../providers/cognito.service";
+import {VoterBallotPage} from "../pages/voter-ballot/voter-ballot";
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(NavController) navCtrl;
+  @ViewChild(Nav) navCtrl;
+
   public loginPage = LoginComponent;
   public homePage = VoterBallotListPage;
   public logoutPage = LogoutComponent;
@@ -47,6 +49,32 @@ export class MyApp {
       this.listenToLoginEvents();
     });
 
+  }
+
+  ngOnInit() {
+    console.log("MyApp: ngAfterViewInit called");
+    this.platform.ready().then(() => {
+      Deeplinks.routeWithNavController(this.navCtrl, {
+        '/ballot/:ballotId': VoterBallotPage
+      }).subscribe((match) => {
+        // match.$route - the route we matched, which is the matched entry from the arguments to route()
+        // match.$args - the args passed in the link
+        // match.$link - the full link data
+        console.log('Successfully matched route route=', JSON.stringify(match));
+
+        var ballotId = match.$link.path.substring(1);
+        console.log("deep loading ballot: "+ballotId);
+        if(this.navCtrl != undefined) {
+          this.navCtrl.setRoot(VoterBallotPage, {"ballotId": ballotId});
+        }else{
+          console.error("this.nav is undefined!")
+        }
+
+      }, (nomatch) => {
+        // nomatch.$link - the full link data
+        console.error('Got a deeplink that didn\'t match', nomatch);
+      });
+    });
   }
 
   openPage(page) {
